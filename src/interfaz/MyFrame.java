a683c37e3e6f34530ef1ad0e349166722cf3105c
Ronaldmolinares <ronald.molinares01@uptc.edu.co>
2023-05-14 21:03:29 -0500

    Avances

--- src/interfaz/MyFrame.java
+++ src/interfaz/MyFrame.java
@@ -1,43 +1,43 @@
 package interfaz;
 
-import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.GridLayout;
 
 public class MyFrame extends JFrame{
 
-    private JPanel welcome;
-    private ImageIcon icon;
+	private JPanel welcome;
+	private JPanel userData;
+	private JPanel game;
+	private ImageIcon icon;
 
-    public MyFrame (){
-        initComponents();
-		this.setSize(600,380);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);  
-        this.setTitle("Tic Tac Toe");
-        icon = new ImageIcon(getClass().getResource("/resources/icono.png"));
+	public MyFrame (){
+		initComponents();
+		this.setSize(800,580);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);  
+		this.setTitle("Tic Tac Toe");
+		icon = new ImageIcon(getClass().getResource("/resources/icono.png"));
 		this.setIconImage(icon.getImage());
-        this.setResizable(false);
-        this.setVisible(true);
-    }
+		this.setResizable(false);
+		this.setVisible(true);
+	}
 
-    public void initComponents(){
-    	welcome = new WelcomePanel();
-    	//welcome.setBorder(BorderFactory.createEmptyBorder(50, 50, 50, 50));
-    	this.add(welcome);
-    	
-    }
+	public void initComponents(){
+		welcome = new WelcomePanel();
+		this.add(welcome);
 
-    public static void main(String[] args) {
-        new MyFrame();
-    }
+//		userData = new UserDataPanel();
+//		this.add(userData);
+
+//		game = new GamePanel();
+//		this.add(game);
+
+	}
+
+	public static void main(String[] args) {
+		new MyFrame();
+	}
 
 
 }
--- src/interfaz/WelcomePanel.java
+++ src/interfaz/WelcomePanel.java
@@ -21,6 +21,7 @@ public class WelcomePanel extends JPanel implements ActionListener{
 		this.setVisible(true);
 		this.setBackground(Color.WHITE);
 		this.setLayout(new GridLayout(3,1));
+		//this.setBorder(BorderFactory.createEmptyBorder(50, 50, 30, 50));
 	}
 
 	private void initComponents() {
@@ -31,7 +32,7 @@ public class WelcomePanel extends JPanel implements ActionListener{
 
 	public void textMessage() {
 		JLabel welcomeLabel = new JLabel("¡Bienvenido!", SwingConstants.CENTER);
-		welcomeLabel.setFont(new Font("Arial", Font.BOLD,34));
+		welcomeLabel.setFont(new Font("Arial Rounded MT", Font.BOLD,64));
 		welcomeLabel.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
 		add(welcomeLabel);
 	}
@@ -39,35 +40,35 @@ public class WelcomePanel extends JPanel implements ActionListener{
 	private void startGame() {
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
 
-	    JButton playButton = new JButton("Play");
-	    playButton.setPreferredSize(new Dimension(140, 32));
-	    playButton.setFont(new Font("Arial", Font.BOLD, 20));
-	    playButton.setBackground(new Color(245, 163, 27));
-	    playButton.setBorder(BorderFactory.createLineBorder(Color.white, 0));
-	    
-	    panel.add(playButton);
-	    panel.setOpaque(false);
-	    add(panel, BorderLayout.CENTER);
+		JButton playButton = new JButton("Play");
+		playButton.setPreferredSize(new Dimension(140, 52));
+		playButton.setFont(new Font("Arial", Font.BOLD, 20));
+		playButton.setBackground(new Color(245, 163, 27));
+		playButton.setBorder(BorderFactory.createLineBorder(Color.white, 0));
+
+		panel.add(playButton);
+		panel.setOpaque(false);
+		add(panel, BorderLayout.CENTER);
 	}
 
 	private void showHistory() {
 		JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-		
-	    JButton historyButton = new JButton("Show History");
-	    historyButton.setPreferredSize(new Dimension(140, 32));
-	    historyButton.setFont(new Font("Arial", Font.BOLD, 20));
-	    historyButton.setBackground(new Color(245, 163, 27));
-	    historyButton.setBorder(BorderFactory.createLineBorder(Color.white, 0));
-	    
-	    panel.add(historyButton);
-	    panel.setOpaque(false);
-	    add(panel, BorderLayout.CENTER);
+
+		JButton historyButton = new JButton("Show History");
+		historyButton.setPreferredSize(new Dimension(140, 52));
+		historyButton.setFont(new Font("Arial", Font.BOLD, 20));
+		historyButton.setBackground(new Color(245, 163, 27));
+		historyButton.setBorder(BorderFactory.createLineBorder(Color.white, 0));
+
+		panel.add(historyButton);
+		panel.setOpaque(false);
+		add(panel, BorderLayout.CENTER);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-		
+
+
 	}
 
 }
